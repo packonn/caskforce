95c76c046cabf4bd84c3fcb19d352190842e1425
packonn <julien.bouloux@gmail.com>
2024-07-28 15:25:21 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    domains: ['tailwindui.com', 'images.unsplash.com'],
+    dangerouslyAllowSVG: true, // Ajoutez cette ligne
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 }
 
 export default withPayload(nextConfig)
